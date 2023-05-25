ba2c1028daba0b05dd046899cc8591bed0dbb765
matheusguermandi <matheus_guermandi@hotmail.com>
2023-05-25 17:20:41 +0100

    chore: update bcrypt implementation imports

--- src/infra/criptography/bcrypt-adapter.ts
+++ src/infra/criptography/bcrypt-adapter.ts
@@ -1,17 +1,15 @@
 import bcrypt from "bcrypt";
-import { Encrypter } from "../../data/protocols/cryptography/encrypter";
+import { Hasher } from "../../data/protocols/cryptography/hasher";
 
-export class BcryptAdapter implements Encrypter {
+export class BcryptAdapter implements Hasher {
   private readonly salt: number;
 
   constructor(salt: number) {
     this.salt = salt;
   }
 
-  async encrypt(value: string): Promise<string> {
-    await bcrypt.hash(value, this.salt);
+  async hash(value: string): Promise<string> {
     const hash = await bcrypt.hash(value, this.salt);
-
     return hash;
   }
 }

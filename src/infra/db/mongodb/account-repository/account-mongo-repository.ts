d7cf7c597e1d432567a5b6412343d8e489bf9e59
matheusguermandi <matheus_guermandi@hotmail.com>
2023-05-29 17:00:42 +0100

    chore: rename infra files to better understand

--- src/infra/db/mongodb/account-repository/account-mongo-repository.ts
+++ src/infra/db/mongodb/account-repository/account-mongo-repository.ts
@@ -1,11 +1,11 @@
 /* eslint-disable @typescript-eslint/brace-style */
 /* eslint-disable @typescript-eslint/indent */
-import { AddAccountRepository } from "../../../../data/protocols/db/add-account-repository";
+import { AddAccountRepository } from "../../../../data/protocols/db/account/add-account-repository";
 import { AddAccountModel } from "../../../../domain/usecases/add-account";
 import { AccountModel } from "../../../../domain/models/account";
 import { MongoHelper } from "../helpers/mongo-helper";
-import { LoadAccountByEmailRepository } from "../../../../data/protocols/db/load-account-by-email-repository";
-import { UpdateAccessTokenRepository } from "../../../../data/protocols/db/update-access-token-repository";
+import { LoadAccountByEmailRepository } from "../../../../data/protocols/db/account/load-account-by-email-repository";
+import { UpdateAccessTokenRepository } from "../../../../data/protocols/db/account/update-access-token-repository";
 
 export class AccountMongoRepository
   implements

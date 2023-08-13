7e2d2efe4a0b8c66f735e87bf430b986b93aaac3
matheusguermandi <matheus_guermandi@hotmail.com>
2023-08-13 22:08:06 +0100

    docs: update login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,24 +1,41 @@
-import { loginPath } from './paths/login-path'
-import { accountSchema } from './schemas/account-schema'
-import { loginParamsSchema } from './schemas/login-params-schema'
+import { loginPath } from './paths'
+import { badRequest, serverError, unauthorized, notFound } from './components'
+import { accountSchema, loginParamsSchema, errorSchema } from './schemas'
 
 export default {
   openapi: '3.0.0',
   info: {
     title: 'Clean Node API',
+    description:
+      'API do curso do Mango para realizar enquetes entre programadores',
     version: '1.0.0'
   },
-  servers: [{
-    url: '/api'
-  }],
-  tags: [{
-    name: 'Login'
-  }],
+  license: {
+    name: 'GPL-3.0-or-later',
+    url: 'https://spdx.org/licenses/GPL-3.0-or-later.html'
+  },
+  servers: [
+    {
+      url: '/api'
+    }
+  ],
+  tags: [
+    {
+      name: 'Login'
+    }
+  ],
   paths: {
     '/login': loginPath
   },
   schemas: {
     account: accountSchema,
-    loginParams: loginParamsSchema
+    loginParams: loginParamsSchema,
+    error: errorSchema
+  },
+  components: {
+    badRequest,
+    serverError,
+    unauthorized,
+    notFound
   }
 }

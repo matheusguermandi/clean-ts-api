d8414dd6635b40d8ee3b522085519891a2af11cd
matheusguermandi <matheus_guermandi@hotmail.com>
2023-08-15 22:14:31 +0100

    docs: add signup api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,51 +1,35 @@
-import { loginPath, surveyPath } from './paths'
-import {
-  badRequest,
-  serverError,
-  unauthorized,
-  notFound,
-  forbidden
-} from './components'
-import {
-  accountSchema,
-  loginParamsSchema,
-  errorSchema,
-  surveyAnswerSchema,
-  surveysSchema,
-  surveySchema,
-  apiKeyAuthSchema
-} from './schemas'
+import { loginPath, surveyPath, signUpPath } from './paths'
+import { badRequest, serverError, unauthorized, notFound, forbidden } from './components'
+import { accountSchema, loginParamsSchema, errorSchema, surveyAnswerSchema, surveysSchema, surveySchema, apiKeyAuthSchema, signUpParamsSchema } from './schemas'
 
 export default {
   openapi: '3.0.0',
   info: {
     title: 'Clean Node API',
+    description: 'API do curso do Mango para realizar enquetes entre programadores',
     version: '1.0.0'
   },
   license: {
     name: 'GPL-3.0-or-later',
     url: 'https://spdx.org/licenses/GPL-3.0-or-later.html'
   },
-  servers: [
-    {
-      url: '/api'
-    }
-  ],
-  tags: [
-    {
-      name: 'Login'
-    },
-    {
-      name: 'Enquete'
-    }
-  ],
+  servers: [{
+    url: '/api'
+  }],
+  tags: [{
+    name: 'Login'
+  }, {
+    name: 'Enquete'
+  }],
   paths: {
     '/login': loginPath,
+    '/signup': signUpPath,
     '/surveys': surveyPath
   },
   schemas: {
     account: accountSchema,
     loginParams: loginParamsSchema,
+    signUpParams: signUpParamsSchema,
     error: errorSchema,
     surveys: surveysSchema,
     survey: surveySchema,

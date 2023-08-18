d010898da36543aab419d214a149c45bfb8afcaa
matheusguermandi <matheus_guermandi@hotmail.com>
2023-08-18 18:40:24 +0100

    docs: add save survey result api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,30 +1,53 @@
-import { loginPath, surveyPath, signUpPath } from './paths'
-import { badRequest, serverError, unauthorized, notFound, forbidden } from './components'
-import { accountSchema, loginParamsSchema, errorSchema, surveyAnswerSchema, surveysSchema, surveySchema, apiKeyAuthSchema, signUpParamsSchema, addSurveyParamsSchema } from './schemas'
+import { loginPath, surveyPath, signUpPath, surveyResultPath } from './paths'
+import {
+  badRequest,
+  serverError,
+  unauthorized,
+  notFound,
+  forbidden
+} from './components'
+import {
+  accountSchema,
+  loginParamsSchema,
+  errorSchema,
+  surveyAnswerSchema,
+  surveysSchema,
+  surveySchema,
+  apiKeyAuthSchema,
+  signUpParamsSchema,
+  addSurveyParamsSchema,
+  saveSurveyParamsSchema,
+  surveyResultSchema
+} from './schemas'
 
 export default {
   openapi: '3.0.0',
   info: {
     title: 'Clean Node API',
-    description: 'API do curso do Mango para realizar enquetes entre programadores',
     version: '1.0.0'
   },
   license: {
     name: 'GPL-3.0-or-later',
     url: 'https://spdx.org/licenses/GPL-3.0-or-later.html'
   },
-  servers: [{
-    url: '/api'
-  }],
-  tags: [{
-    name: 'Login'
-  }, {
-    name: 'Enquete'
-  }],
+  servers: [
+    {
+      url: '/api'
+    }
+  ],
+  tags: [
+    {
+      name: 'Login'
+    },
+    {
+      name: 'Enquete'
+    }
+  ],
   paths: {
     '/login': loginPath,
     '/signup': signUpPath,
-    '/surveys': surveyPath
+    '/surveys': surveyPath,
+    '/surveys/{surveyId}/results': surveyResultPath
   },
   schemas: {
     account: accountSchema,
@@ -34,7 +57,9 @@ export default {
     error: errorSchema,
     surveys: surveysSchema,
     survey: surveySchema,
-    surveyAnswer: surveyAnswerSchema
+    surveyAnswer: surveyAnswerSchema,
+    saveSurveyParams: saveSurveyParamsSchema,
+    surveyResult: surveyResultSchema
   },
   components: {
     securitySchemes: {

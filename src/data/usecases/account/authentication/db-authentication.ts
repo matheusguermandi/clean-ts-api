1c6b2902c29ffb59b0a9129112cdd319a76a2c50
matheusguermandi <matheus_guermandi@hotmail.com>
2023-09-15 23:29:17 +0100

    chore: update authentication use case implementation to return authentication model

--- src/data/usecases/account/authentication/db-authentication.ts
+++ src/data/usecases/account/authentication/db-authentication.ts
@@ -1,4 +1,12 @@
-import { Authentication, AuthenticationParams, LoadAccountByEmailRepository, HashComparer, Encrypter, UpdateAccessTokenRepository } from './db-authentication-protocols'
+import {
+  Authentication,
+  AuthenticationParams,
+  LoadAccountByEmailRepository,
+  HashComparer,
+  Encrypter,
+  UpdateAccessTokenRepository,
+  AuthenticationModel
+} from './db-authentication-protocols'
 
 export class DbAuthentication implements Authentication {
   constructor (
@@ -8,14 +16,27 @@ export class DbAuthentication implements Authentication {
     private readonly updateAccessTokenRepository: UpdateAccessTokenRepository
   ) {}
 
-  async auth (authenticationParams: AuthenticationParams): Promise<string> {
-    const account = await this.loadAccountByEmailRepository.loadByEmail(authenticationParams.email)
+  async auth (
+    authenticationParams: AuthenticationParams
+  ): Promise<AuthenticationModel> {
+    const account = await this.loadAccountByEmailRepository.loadByEmail(
+      authenticationParams.email
+    )
     if (account) {
-      const isValid = await this.hashComparer.compare(authenticationParams.password, account.password)
+      const isValid = await this.hashComparer.compare(
+        authenticationParams.password,
+        account.password
+      )
       if (isValid) {
         const accessToken = await this.encrypter.encrypt(account.id)
-        await this.updateAccessTokenRepository.updateAccessToken(account.id, accessToken)
-        return accessToken
+        await this.updateAccessTokenRepository.updateAccessToken(
+          account.id,
+          accessToken
+        )
+        return {
+          accessToken,
+          name: account.name
+        }
       }
     }
     return null

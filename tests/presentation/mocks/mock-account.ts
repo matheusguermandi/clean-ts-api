28b78a5579bea7e7bdb998921909a37d25b309db
matheusguermandi <matheus_guermandi@hotmail.com>
2023-10-01 11:44:32 +0100

    chore: update domain account mock

--- tests/presentation/mocks/mock-account.ts
+++ tests/presentation/mocks/mock-account.ts
@@ -1,40 +1,38 @@
-import { AddAccount, Authentication, AuthenticationParams, LoadAccountByToken } from '@/domain/usecases'
-import { AccountModel, AuthenticationModel } from '@/domain/models'
-import { mockAccountModel } from '@/tests/domain/mocks'
+import { AddAccount, Authentication, LoadAccountByToken } from '@/domain/usecases'
 
 import faker from 'faker'
 
 export class AddAccountSpy implements AddAccount {
-  isValid = true
+  result = true
   addAccountParams: AddAccount.Params
 
   async add (account: AddAccount.Params): Promise<AddAccount.Result> {
     this.addAccountParams = account
-    return this.isValid
+    return this.result
   }
 }
 
 export class AuthenticationSpy implements Authentication {
-  authenticationParams: AuthenticationParams
+  authenticationParams: Authentication.Params
   authenticationModel = {
     accessToken: faker.random.uuid(),
     name: faker.name.findName()
   }
 
-  async auth (authenticationParams: AuthenticationParams): Promise<AuthenticationModel> {
+  async auth (authenticationParams: Authentication.Params): Promise<Authentication.Result> {
     this.authenticationParams = authenticationParams
     return this.authenticationModel
   }
 }
 
 export class LoadAccountByTokenSpy implements LoadAccountByToken {
-  accountModel = mockAccountModel()
+  result = { id: faker.random.uuid() }
   accessToken: string
   role: string
 
-  async load (accessToken: string, role?: string): Promise<AccountModel> {
+  async load (accessToken: string, role?: string): Promise<LoadAccountByToken.Result> {
     this.accessToken = accessToken
     this.role = role
-    return this.accountModel
+    return this.result
   }
 }

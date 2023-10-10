2b1268656f2e30d56933c26d29c8b70b471007f8
matheusguermandi <matheus_guermandi@hotmail.com>
2023-10-10 17:13:13 +0100

    chore: remove unneeded await statement from save survey result use case

--- src/data/usecases/db-save-survey-result.ts
+++ src/data/usecases/db-save-survey-result.ts
@@ -10,6 +10,6 @@ export class DbSaveSurveyResult implements SaveSurveyResult {
 
   async save (data: SaveSurveyResultParams): Promise<SurveyResultModel> {
     await this.saveSurveyResultRepository.save(data)
-    return await this.loadSurveyResultRepository.loadBySurveyId(data.surveyId, data.accountId)
+    return this.loadSurveyResultRepository.loadBySurveyId(data.surveyId, data.accountId)
   }
 }

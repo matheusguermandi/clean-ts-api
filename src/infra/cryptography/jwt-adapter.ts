82378a4df15b7004ca2da7122b31e3128bdc682a
matheusguermandi <matheus_guermandi@hotmail.com>
2023-10-10 17:15:46 +0100

    chore: update cryptography adapters implementation

--- src/infra/cryptography/jwt-adapter.ts
+++ src/infra/cryptography/jwt-adapter.ts
@@ -6,7 +6,7 @@ export class JwtAdapter implements Encrypter, Decrypter {
   constructor (private readonly secret: string) {}
 
   async encrypt (plaintext: string): Promise<string> {
-    return await jwt.sign({ id: plaintext }, this.secret)
+    return jwt.sign({ id: plaintext }, this.secret)
   }
 
   async decrypt (ciphertext: string): Promise<string> {
